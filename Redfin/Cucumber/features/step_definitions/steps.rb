5b5e7348a58f68e6e91a95a83dea7f260c74fef1
Brian Dang <bdang@securityfirstcorp.com>
2018-11-21 20:55:50 -0800

    updated to reflect new feature test for estate listing

--- Redfin/Cucumber/features/step_definitions/steps.rb
+++ Redfin/Cucumber/features/step_definitions/steps.rb
@@ -1,18 +1,41 @@
 require "watir"
 require_relative "../../../home_page"
+require_relative "../../../estate_listing"
 
 Given("{string} and {string}") do |username, password|
-  @username = username; @password = password # Write code here that turns the phrase above into concrete actions
+    @username = username; @password = password # Write code here that turns the phrase above into concrete actions
 end
 
 When("I login to Redfin with Google Chrome") do
-  @browser = HomePage.new
-  @my_login_status = @browser.sign_in(@username, @password)
-  puts @my_login_status
+    @homepage = HomePage.new
+    @my_login_status = @homepage.sign_in(@username, @password)
+    puts @my_login_status
    # Write code here that turns the phrase above into concrete actions
 end
 
 Then("I should be {string}") do |result|
-  expect(@my_login_status).to eq(result)
+    expect(@my_login_status).to eq(result)
   # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I want {int} beds and {int} and at the max price of '$1M' in {string}") do |beds, baths, city|
+    @estatepage = EstatePage.new
+    current_page = @estatepage.enter_search(city)
+    if current_page.include? city
+        @estatepage.max_price
+        @estatepage.open_filters
+        @estatepage.max_beds(beds)
+        @estatepage.num_baths(baths)
+        @estatepage.apply_filters
+    else
+        @my_search_status ||= 'fail'
+    end
+end
+
+When("I search for listings on Redfin with my criteria") do
+    @my_search_status ||= @estatepage.check_results # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be {string} and only see results based on my criteria") do |results|
+    expect(@my_search_status).to eq(results) # Write code here that turns the phrase above into concrete actions
 end
