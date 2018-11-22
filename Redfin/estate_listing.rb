ee2df67d790b45b92d932214633956079fea2ef7
Brian Dang <bdang@securityfirstcorp.com>
2018-11-21 18:56:17 -0800

    Updated to add in filters for max price/beds/baths

--- Redfin/estate_listing.rb
+++ Redfin/estate_listing.rb
@@ -10,5 +10,21 @@ class Estate_Page
         @browser.button(:data_rf_test_name => "searchButton", :title => "Search").click
     end
 
-    def change_max(price)
+    def max_price(price)
+        @browser.span(:data_rf_test_name => "Select", :class ["quickMaxPrice", "withFlyout", "withOptions", "mounted", "field", "select", "Select", "clickable"]).click
+        @browser.div(:class => ["Flyout", "standard", "v83", "position-below", "alignment-left", "no-padding", "no-adjustment", "none", "transparent", "standard"]).div(:class => "option", :text => price).when_present.click
+    end
+
+    def open_filters
+        @browser.button(:data_rf_test_id => "filterButton").click
+    end
+
+    def max_beds(beds)
+        @browser.span(:data_rf_test_name => "Select", :class => ["maxBeds", "withFlyout", "withOptions", "mounted", "field", "select", "Select", "clickable"]).click
+        @browser.div(:class => ["Flyout", "standard", "v83", "position-below", "alignment-left", "no-padding", "no-adjustment", "none", "transparent", "standard"]).div(:class => "option", :text => beds).when_present.click
+    end
+
+    def num_baths(baths)
+        @browser.span(:data_rf_test_name => "Stepper", :class => ["withFlyout", "withOptions", "field", "select", "Stepper", "Select", "clickable", "baths"]).click
+        @browser.div(:class => ["Flyout", "standard", "v83", "position-below", "alignment-left", "no-padding", "no-adjustment", "none", "transparent", "standard"]).div(:class => "option", :text => baths + "+").when_present.click
     end
